815c9fc87f11e870db3ac69b3386a9b11bff48f1
GBielke25 <831366@mystma.org>
2023-03-24 14:28:39 -0500

    3

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 def topFiveMovies(a , b , c , d , e):
-    return("5 Iron man 1 " , "4 How To Train Your Dragon 2 " , "3 Avatar" , "2 Avatar the airbender " , "1 How To Train Your Dragon 1 ")
+    return(a + b + c + d + e)
+print("5 Iron man 1 " , "4 How To Train Your Dragon 2 " , "3 Avatar" , "2 Avatar the airbender " , "1 How To Train Your Dragon 1 ")
